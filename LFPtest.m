0202d720cde26c137a69e26be46f7ee7c2cd8753
John A. Thompson <potatoking.john@gmail.com>
2014-10-17 17:04:21 -0600

    Update files

--- LFPtest.m
+++ LFPtest.m
@@ -20,11 +20,18 @@ fName2use = fNames{fNindex};
 
 load(fName2use);
 
-grandCV = mean([abs(std(CLFP1))/abs(mean(CLFP1)) ,...
-               abs(std(CLFP2))/abs(mean(CLFP2)) ,...
-               abs(std(CLFP3))/abs(mean(CLFP3))]);
+if ~exist('CLFP1','var')
+    grandCV = 0;
+else
+    grandCV = mean([abs(std(CLFP1))/abs(mean(CLFP1)) ,...
+        abs(std(CLFP2))/abs(mean(CLFP2)) ,...
+        abs(std(CLFP3))/abs(mean(CLFP3))]);
+end
            
-if grandCV <= 0.3
+           
+if grandCV <= 0.3 || sum([abs(std(CLFP1))/abs(mean(CLFP1)) <= 0.3,...
+                          abs(std(CLFP2))/abs(mean(CLFP2)) <= 0.3,...
+                          abs(std(CLFP3))/abs(mean(CLFP3)) <= 0.3]) >= 2;
     lfpID = 'NO';
     lfpBool = 0;
 else
