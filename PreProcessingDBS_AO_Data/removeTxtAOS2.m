34da04dcf673d3bd31e6786f96dd52f8000851dc
John A. Thompson <john.a.thompson@cuanschutz.edu>
2019-10-31 10:28:25 -0600

    update files and add new file

--- PreProcessingDBS_AO_Data/removeTxtAOS2.m
+++ PreProcessingDBS_AO_Data/removeTxtAOS2.m
@@ -2,30 +2,31 @@ function [] = removeTxtAOS2(year)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
 
-
 mainDir = ['X:\S2_AOUnFMatlabData_S2',filesep,num2str(year)];
 
-
 dateDirFs = getDirFolders(mainDir);
+
+dsh = filesep;
 
 for ddi = 1:length(dateDirFs)
     
     tmpDateD = [mainDir, filesep, dateDirFs{ddi}];
     
-    outTxtNames = getTxtNames(tmpDateD);
+    ckSet = checkSets(tmpDateD);
     
-    if isempty(outTxtNames)
-        disp('Files have already been removed!')
-        continue
-    else
-        cd(tmpDateD)
+    if ckSet
         
-        for di = 1:length(outTxtNames)
-            delTmp = outTxtNames{di};
-            delete(delTmp)
+        setFolds2 = getDirFolders(tmpDateD);
+        for setI = 1:length(setFolds2)
+            tmpSet = [tmpDateD , dsh , setFolds2{setI}];
+            delTxt(tmpSet)
         end
         
+    else
+        delTxt(tmpDateD)
+        
     end
+    
     
 end
 
@@ -39,10 +40,68 @@ end
 function [outTxtNames] = getTxtNames(mainDir)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
 cd(mainDir)
 getCons = dir('*.txt');
 outTxtNames = {getCons.name};
 
+end
+
+
+
+
+
+function [outMatNames] = getMatNames(mainDir,matflag)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+cd(mainDir)
+
+if matflag
+    getCons = dir('*.mat');
+else
+    getCons = [dir('*.mat'); dir('*.txt')];
+end
+
+outMatNames = {getCons.name};
+
+end
+
+
+
+function checkSet = checkSets(DIRloc)
+
+[outMatNames] = getMatNames(DIRloc,1);
+
+if isempty(outMatNames)
+    checkSet = 1;
+else
+    checkSet = 0;
+end
+
+end
+
+
+
+
+
+
+
+function [] = delTxt(tmpDIR)
+
+    outTxtNames = getTxtNames(tmpDIR);
+    outTxtNames = outTxtNames(~ismember(outTxtNames,{'lfp_yes.txt','NOAO.txt'}));
+    
+    if isempty(outTxtNames)
+        disp('Files have already been removed!')
+        return
+    else
+        cd(tmpDateD)
+        
+        for di = 1:length(outTxtNames)
+            delTmp = outTxtNames{di};
+            delete(delTmp)
+        end
+        
+    end
 
 end
