6e9df3bceba0d4bd6eb0273430a96d42ba5704cc
John A. Thompson <john.a.thompson@cuanschutz.edu>
2020-07-11 12:33:13 -0600

    udpate code

--- PreProcessingDBS_AO_Data/AO_IO_Ephys_processing.m
+++ PreProcessingDBS_AO_Data/AO_IO_Ephys_processing.m
@@ -1,26 +1,30 @@
 function [] = AO_IO_Ephys_processing(step , year , driveLetter, study , date)
 
 
-if nargin == 4
-    
+if nargin == 3
     date2use = nan;
-    
+    study2use = 'ET-DK';
+elseif nargin == 4
+    study2use = study;
+    date2use = nan;
 else
-    
     date2use = date;
-    
+    study2use = study;
 end
 
 
 switch step
     
+    % case 1 % loop through S1 year and add folders to S2 that are missing
     case 1
-        place2SetsTxts_AOS2(year , driveLetter)
+        preSetAOC_folds(year, driveLetter)
     case 2
+        place2SetsTxts_AOS2(year , driveLetter)
+    case 3
         initializeS2_S3(year , driveLetter)
-    case 3 % NEED TO FIX SETS
-        postAOCleanUp(study , year , date2use , driveLetter)
     case 4 % NEED TO FIX SETS
+        postAOCleanUp(study2use , year , date2use , driveLetter)
+    case 5 % NEED TO FIX SETS
         resetS3_S4_crash(year , date2use , driveLetter)
 end
 
