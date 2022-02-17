cf4d33166195ca430ac2b7d85cf85ac1aaf99a2b
John Thompson <john.a.thompson@cuanschutz.edu>
2022-02-17 16:46:41 -0700

    update

--- PreProcessingDBS_AO_Data/AO_IO_Ephys_processing.m
+++ PreProcessingDBS_AO_Data/AO_IO_Ephys_processing.m
@@ -16,11 +16,11 @@ end
 switch step
     
     % case 1 % loop through S1 year and add folders to S2 that are missing
-    case 1
+    case 1 % RUN WHEN ONLY .MPX files are available
         preSetAOC_folds(year, driveLetter)
-    case 2
+    case 2 % RUN AFTER CONVERTING MPX to MAT in S2
         place2SetsTxts_AOS2(year , driveLetter)
-    case 3
+    case 3 % RUN AFTER STEP 2
         initializeS2_S3(year , driveLetter)
     case 4 % NEED TO FIX SETS
         postAOCleanUp(study2use , year , date2use , driveLetter)
